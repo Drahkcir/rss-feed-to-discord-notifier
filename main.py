3888edd288b4111128b4e89edd85cb890451f9f8
Drahkcir <Drahkcir@protonmail.com>
2024-05-27 22:19:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+import re
+from discord_webhook import DiscordWebhook,DiscordEmbed
+
+WEBHOOK_URL="https://discord.com/api/webhooks/1231570996905906287/pNOMLekr2tF81E5iwFOPvqeGBI_dLwVm9TUcr_zu8Hxlf-dnrMe_cDtczbdJqrI1A1Hc"
+
+
+class Webhook:
+
+    # constructor
+    def __init__(self,title='CERT-FR',description='notifier'):
+        self.webhook = DiscordWebhook(
+            url=WEBHOOK_URL, 
+            rate_limit_retry=True,
+            description=description,
+            title=title
+            )
+        return
+        
+
+    def send_message(self):
+        pass
+
+    def edit_message(self, content):
+        self.webhook.content=content
+
+
+    def edit_embeded(self):
+        # create embed object for webhook
+        # you can set the color as a decimal (color=242424) or hex (color="03b2f8") number
+        embed = DiscordEmbed(title="CERT-FR", description="Lorem ipsum dolor sit", color="03b2f8")
+
+        # add embed object to webhook
+        self.webhook.add_embed(embed)
+
+    def sendMessages(self):
+        response = self.webhook.execute()
+        
+
+
+obj1=Webhook()
+obj1.edit_message('about time you did it !')
+obj1.edit_embeded()
+obj1.sendMessages()

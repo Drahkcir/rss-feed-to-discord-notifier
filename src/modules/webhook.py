06ea77f08232d3ab5b62adf163aa2b1ef83fe539
Drahkcir <Drahkcir@protonmail.com>
2024-06-03 18:31:21 +0200

    work on webhook class to be generic and easy to grasp

--- src/modules/webhook.py
+++ src/modules/webhook.py
@@ -1,31 +1,38 @@
+import configparser
+
+from . import config
 from discord_webhook import DiscordWebhook,DiscordEmbed
+
+
 
 class Webhook:
 
     # constructor
-    def __init__(self,url,title='CERT-FR',description='notifier'):
-        self.webhook = DiscordWebhook(
-            url=url,
-            rate_limit_retry=True,
-            description=description,
-            title=title
-            )
-        return
+    def __init__(self,url:str, title:str, description:str='notifier'):
+        self.webhook = DiscordWebhook(url,rate_limit_retry=True)
+        self.embed=None
+        
+        
+    def create_embeded(self,config:configparser):
 
-    def send_message(self):
-        pass
-
-    def edit_message(self, content):
-        self.webhook.content=content
-
-
-    def edit_embeded(self):
-        # create embed object for webhook
         # you can set the color as a decimal (color=242424) or hex (color="03b2f8") number
-        embed = DiscordEmbed(title="CERT-FR", description="Lorem ipsum dolor sit", color="03b2f8")
+        self.embed = DiscordEmbed(title="CERT-FR", description="Lorem ipsum dolor sit", color="03b2f8")
+
+        self.embed.set_author(name="", url="", icon_url="author icon url")        
+        self.embed.set_image(url="")
+        self.embed.set_thumbnail(url="")
+        self.embed.set_footer(text="footer text", icon_url="")
+
+        # set timestamp (default is now) accepted types are int, float and datetime
+        self.embed.set_timestamp()
 
         # add embed object to webhook
-        self.webhook.add_embed(embed)
+        self.webhook.add_embed(self.embed)
 
-    def sendMessages(self):
+
+    def send_embeded(self):
+        
+        self.webhook.execute(remove_embeds=True)
+
+    def send_message(self):
         response = self.webhook.execute()

787955bc3442845f0e43c197d5939c981d7172b2
Drahkcir <Drahkcir@protonmail.com>
2024-06-10 17:41:30 +0200

    config update

--- src/modules/config.py
+++ src/modules/config.py
@@ -3,16 +3,18 @@ import configparser
 
 class configLoader:
     def __init__(self,file:str = 'config.ini' ,section:str = 'DEFAULT'):
-        conf = configparser.ConfigParser()
-        conf.read(file)
+        self.conf = configparser.ConfigParser()
+        self.conf.read(file)
 
-    def check_init_variables():
+    def check_init_variables(self):
         #TODO check that every variable needed is well initialized
         pass
         
-    def save_conf_status():
+    def save_conf_status(self):
         #TODO write the init file with the current configuration that we (keep track of wich item in the feed we processed) 
+        self.conf.write(self)
         pass
 
-    def pretty_print():
+    def pretty_print(self):
+        print(self.conf.items)
         pass

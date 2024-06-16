259f87bc4fb21b079311fc709b1a9676e1141d68
Drahkcir <Drahkcir@protonmail.com>
2024-06-16 11:43:16 +0200

    first draft get_unprocessed_entry

--- src/modules/rss_feed.py
+++ src/modules/rss_feed.py
@@ -22,10 +22,27 @@ class feed_obj:
         self.get_feed_general_info()
         
 
-    def get_unprocessed_entry(self):
-        pass
+    def get_unprocessed_entry(self,last_processed_date:datetime):
+        unprocessed_entries={}
+        if last_processed_date:
+            for item in self.items:
+                date_field = ''
+                if 'pubDate' in item.keys():
+                    date_field = 'pubDate'
+                elif 'published' in item.keys():
+                    date_field = 'pubDate'
+                else:
+                    logger.error('unable to identify item {item} creation date field')
+                    continue
 
-    def identify_author():
+                # TODO parse date of field
+            
+                if item[date_field] < last_processed_date :
+                    unprocessed_entries.append(item)
+            else:
+                unprocessed_entries = self.items()
+        return unprocessed_entries
+
         pass
 
     def get_feed_general_info(self):

e4544923f28d85f8ef537a735bb24beb7c13bd0a
Drahkcir <40502609+Drahkcir@users.noreply.github.com>
2024-07-10 22:02:02 +0200

    Update setup_service.sh usage message

--- service/setup_service.sh
+++ service/setup_service.sh
@@ -6,12 +6,16 @@ function usage(){
 
 echo -e <<EOF
 
-usage: ${SCRIPT_NAME}
+usage: ${SCRIPT_NAME} [-h|--help] [-v|--verbose] 
 
 description: 
-    
+        this script will move the systemd units from the ./ servce folder to the /etc/systemd/system/ folder 
+        and perform a daemon-reload to load the new/updated units.
+
+arguments: 
+    -h,--help       print this message 
+    -v,--verbose    output more info on actions performed
 
 EOF
-  
 
 }
